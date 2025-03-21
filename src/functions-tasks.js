f5528c31ea4c0ed9a706512aedaf7a82ad10d7e5
jackal7819 <jackal3166@gmail.com>
2025-03-21 17:02:55 +0200

    feat: pass next 2 tests (11/11 completed)

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -197,8 +197,8 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => fn(...args1, ...args2);
 }
 
 /**
@@ -218,8 +218,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom;
+  return () => {
+    const current = id;
+    id += 1;
+    return current;
+  };
 }
 
 module.exports = {
