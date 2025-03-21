5d938af9768868751e548ca55b00de39d66a2d5b
jackal7819 <jackal3166@gmail.com>
2025-03-21 16:56:08 +0200

    feat: pass next 3 tests (9/11 completed)

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -136,8 +136,19 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function retryWrapper() {
+    let tries = attempts;
+    while (tries > 0) {
+      try {
+        return func();
+      } catch (error) {
+        tries -= 1;
+        if (tries === 0) throw error;
+      }
+    }
+    return undefined;
+  };
 }
 
 /**
@@ -163,8 +174,14 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function loggerWrapper(...args) {
+    const argString = args.map((arg) => JSON.stringify(arg)).join(',');
+    logFunc(`${func.name}(${argString}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${argString}) ends`);
+    return result;
+  };
 }
 
 /**
